e57a531fb3247f530d2160520f316a44f43622df
xFinVi <111379135+xFinVi@users.noreply.github.com>
2024-07-05 23:31:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ const generateThumbnailDocs = (req, res) => {
     name: "generateThumbnail",
     description: "Generate a thumbnail from an image URL",
     input: {
-      type: "object",
-      description: "Object with an imageUrl property",
-      example: { input: { imageUrl: "https://example.com/image.jpg" } }
+      type: "string",
+      description: "Generate a thumbnail from an image URL",
+      example: { input: "https://images.unsplash.com/photo-1716847214612-e2c2f3771d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  }
     },
     output: {
       type: "image/jpeg",
