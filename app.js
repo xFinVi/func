4a175316c2cfb39a1db1de8a1ec3cf35e8963319
xFinVi <111379135+xFinVi@users.noreply.github.com>
2024-07-06 02:03:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const multer = require('multer');
 const sharp = require('sharp');
-const upload = multer();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-// Middleware to parse urlencoded form data
-app.use(express.urlencoded({ extended: true }));
-app.use(upload.any()); 
-
 
 // Function to generate a thumbnail from an image URL
 const generateThumbnail = async (req, res) => {
@@ -48,29 +42,31 @@ const generateThumbnailDocs = (req, res) => {
   const docs = {
     name: "generateThumbnail",
     description: "Generate a thumbnail from an image URL",
-    input: {
-      type: "object",
-      properties: {
-        input: {
+    input: {   
           type: "string",
           description: "URL of the image to generate a thumbnail from",
-          example: "https://images.unsplash.com/photo-1716847214612-e2c2f3771d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }
-      }
+          example: "https://images.unsplash.com/photo-1716847214612-e2c2f3771d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"     
     },
     output: {
       type: "string",
       description: "URL of the resized image in 100x100 thumbnail format",
-      example: "https://images.unsplash.com/photo-1716847214612-e2c2f3771d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.png"
-
+      example: "https://images.unsplash.com/photo-1716847214612-e2c2f3771d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg"
     }
   };
 
   res.json(docs);
 };
 
-// Define routes
-app.post('/generateThumbnail', generateThumbnail);
+// Custom middleware to ensure JSON content type
+const enforceJsonMiddleware = (req, res, next) => {
+  if (!req.is('application/json')) {
+    return res.status(400).json({ error: 'Invalid content type. Expected application/json.' });
+  }
+  next();
+};
+
+// Apply middleware to enforce JSON content type only for generateThumbnail endpoint
+app.post('/generateThumbnail', enforceJsonMiddleware, generateThumbnail);
 app.get('/generateThumbnail', generateThumbnailDocs); // Changed route path to avoid conflict
 
 // Start the server
