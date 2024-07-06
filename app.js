b3eb57944a1b1e5d9b8c11883c1fb7866560801a
xFinVi <111379135+xFinVi@users.noreply.github.com>
2024-07-06 09:10:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const sharp = require('sharp');
 const axios = require('axios');
-const multer
-
+const multer = require('multer');
+const upload = multer();
 const app = express();
 const port = process.env.PORT || 3000;
 
