c4517b0a188d2d547a137bc90f42408fed556918
xFinVi <111379135+xFinVi@users.noreply.github.com>
2024-07-06 22:19:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const getDocs = (req, res) => {
     input: {
       type: "string",
       description: "URL of the image to convert to base64",
-      example: "https://example.com/image.jpg"
+      example: "https://plus.unsplash.com/premium_photo-1717529138029-5b049119cfb1?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     output: {
       type: "string",
